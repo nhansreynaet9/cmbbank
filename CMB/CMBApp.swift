da996a8e3dd7db41b465430ff16d8502671de2a3
Nhan Sreynaet <nhan1523@icloud.com>
2026-05-15 13:00:06 +0700

    Initial Commit

---
+++ CMB/CMBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMBApp.swift
+//  CMB
+//
+//  Created by Sreynaet.NHAN on 15/5/26.
+//
+
+import SwiftUI
+
+@main
+struct CMBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
